e9a1149cafb60c7a0114529e741b37b937b69232
Armon Carigiet <armonc@student.ethz.ch>
2021-12-21 06:29:50 +0100

    benchmark script

---
+++ benchmark.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from pathlib import Path
+
+
+scenes = [ "benchmark_configurations/scene/conference.txt", 
+           "benchmark_configurations/scene/fairyforest.txt", 
+           "benchmark_configurations/scene/sibenik.txt",  
+           "benchmark_configurations/scene/sanmiguel.txt" ]
+
+setup = "benchmark_configurations/setup/benchmark_batch_50.txt"
+
+kernels = [ "./benchmark_configurations/kernels/warmup.txt",
+            "./benchmark_configurations/kernels/stackless.txt",
+            "./benchmark_configurations/kernels/reference_kernels.txt", 
+            "./benchmark_configurations/kernels/early_break.txt" ]
+
+
+for scene in scenes:
+    exec = "python .\\single_benchmark.py " + setup + " " + scene + " " + " ".join(kernels)
+    os.system(exec)
